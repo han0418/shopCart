b072b514b1cc088e80091c1711e26b61ab8535b3
han0418 <lovehan0418@gmail.com>
2024-02-20 20:08:28 +0800

    調整主畫面，列表添加回控制台按鈕

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,3 +2,10 @@
 @section('content')
     <h1>Product</h1>
 @endsection
+
+@section('my_menu')
+    <li class="nav-item">
+        <a class="nav-link" href="/home">回控制台</a>
+    </li>
+    @parent
+@stop
