08aa6bf1c5375087731905dea115bdd4a2ccf5e9
han0418 <lovehan0418@gmail.com>
2024-02-22 00:02:41 +0800

    添加取得商品列表路由

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', 'ProductController@index')->name('index');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
