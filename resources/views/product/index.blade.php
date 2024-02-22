55b1d0cac3460a736374c3276211805919340133
han0418 <lovehan0418@gmail.com>
2024-02-22 11:21:49 +0800

    添加商品列表可視圖

---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('content')
+    <h1>Product</h1>
+    <div class="card-deck">
+        @forelse($products->where('on_sale', true) as $product)
+            <div class="card mb-4">
+                    <img src="{{ $product->image_url }}" class="card-img-top" alt="{{ $product->title }}">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $product->title }}</h5>
+                </div>
+                <div class="card-footer text-center">
+                    ${{ $product->price }}
+                    <button class="btn btn-primary btn-add-to-cart" data-id="{{ $product->id }}">加入購物車</button>
+                </div>
+            </div>
+        @empty
+            <div class="card mb-4">
+                <div class="card-body">
+                    <h1 class="card-title">Your shopping cart is empty.</h1>
+                </div>
+            </div>
+        @endforelse
+    </div>
+    <input type="hidden" name="amount" value="1">
+@endsection
