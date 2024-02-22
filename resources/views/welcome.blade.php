88cd0f6f5c640ce9ab5b5d9fa7f330a48f658d4c
han0418 <lovehan0418@gmail.com>
2024-02-22 21:47:27 +0800

    調整首頁

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,16 +3,13 @@
 <div class="container">
     <div class="row justify-content-center align-items-center">
         <div class="col-md-8 text-center">
-                <h1 class="display-4">Welcome</h1>
-            </div>
-    </div>
-
-    <div class="text-center">
-        <a class="btn btn-primary btn-go-shopping" href="/product">Go Shopping !</a>
+            <h1 class="display-4">Welcome</h1>
+            <p class="lead">Start your shopping journey now!</p>
+            <a class="btn btn-primary btn-lg" href="/product">Go Shopping</a>
+        </div>
     </div>
 </div>
 @endsection
-
 
 @section('my_menu')
     <li class="nav-item">
