2b85e7d797593bce37d2f50df75ac15e276e7ab5
han0418 <lovehan0418@gmail.com>
2024-03-13 01:26:08 +0800

    調整訂單 model，添加取得訂單編號方法

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Order extends Model
 {
+    protected $fillable = [
+        'order_id', 'user_id', 'address', 'total', 'closed'
+    ];
+
     public function user()
     {
         return $this->belongsTo('App\Models\User');
@@ -14,5 +19,10 @@ class Order extends Model
     public function items()
     {
         return $this->hasMany('App\Models\OrderItem');
+    }
+
+    public function getOrderId()
+    {
+        return time().str_replace("-", "",substr(Str::uuid()->toString(), 0,10));
     }
 }
